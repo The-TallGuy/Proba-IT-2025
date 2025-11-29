c0bb5df0f5b4753dd9d9e81fcdc1868267f2eea7
Costin Fulina <73236390+The-TallGuy@users.noreply.github.com>
2025-11-30 00:01:11 +0200

    Add user authentication

---
+++ backend/models/user.model.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true
+            /* Also need to implement a check that they have:
+                - An '@'
+                - A credible domain (find a list somewhere online for big email providers' handles) (maybe give custom looks to those with specific institutional emails)
+                - A '.com' or '.ro' at the end (might be implemented via the domains at step 2 anyway, but doesn't hurt to check)
+            */
+        },
+
+        password: {
+            type: String,
+            required: true
+        },
+
+        username: {
+            type: String,
+            required: true,
+            unique: true
+            // Could give out special cosmetics to those with funny names, in all lowercase (Important Lsac members or faculty staff)
+        },
+
+        // Optional
+
+        fullName: {
+            type: String
+        },
+
+        dateOfRegistration: {
+            type: Date,
+            default: Date.now
+        } //,
+
+        // Bonus
+
+        /*
+        // Currency for the site. Everybody gets one for each member that signs up after them, and they can give one to a user they appreciate, every [time interval I could not come up with yet]
+
+        miciAvailable: {
+            type: Number,
+            default: 5
+        },
+
+        Also: miciGiven; a way to track who you gave mici to. (Bad idea to write a DB Schema after a train ride)
+        */
+    }
+);
+
+module.exports = mongoose.model('User', userSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,64 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
+const User = require('./models/user.model.js')
+
 const app = express()
+app.use(express.json())
 const port = 3000
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 app.get('/', (req, res) => {
-  res.send('cowsay Hello')
+  res.send('Hello, World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+// MONGODB SETUP
+
+mongoose.connect(process.env.MONGO_URL).then( () => {
+  console.log("Connection was successful!");
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  });
+}).catch(() => {
+  console.log("Connection failed!")
 })
+
+//
+
+app.post('/api/users/signup', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = await User.create({username: req.body.username, email: req.body.email, password: hashedPassword});
+    res.status(201).json(user).send();
+    // Unique email and user check implemented on the db side (see user model)
+  }
+  catch (error) {
+    res.status(500).send(error.message)
+  }
+});
+
+app.post('/api/users/login', async (req, res) => {
+  try {
+    let user;
+    // console.log("Request body:", req.body); // Log the request body
+    if (req.body.username) {
+      user = await User.findOne({ username: req.body.username }).exec();
+      // console.log("Searching by username:", req.body.username); // Log the username
+    } else {
+      user = await User.findOne({ email: req.body.email }).exec();
+      // console.log("Searching by email:", req.body.email); // Log the email
+    }
+    if (!user) return res.status(404).send("User does not exist!");
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.status(202).send("Login successful!");
+    } else {
+      res.status(423).send("Incorrect password!");
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+//
